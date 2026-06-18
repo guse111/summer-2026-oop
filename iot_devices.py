fd0affb03cfa5abde69e0735efdef9a1bf8fea12
guse111 <evguse174@gmail.com>
2026-06-18 18:26:01 +1000

    SmartBulb

--- iot_devices.py
+++ iot_devices.py
@@ -1,24 +1,45 @@
 from abc import abstractmethod, ABC
 from dataclasses import dataclass
 
+class DeviceError(Exception):
+    pass
+
 @dataclass
 class SmartDevice(ABC):
     name: str
     is_on: bool = False
 
-    #@abstractmethod
+    @abstractmethod
     def toggle(self):
-        if self.is_on: self.is_on = False
-        else: self.is_on = True
-
+        None
     def __str__(self):
-        return super().__str__()
-
+        if self.is_on:
+            return f"Устройство {self.name} включено"
+        else: 
+            return f"Устройство {self.name} выключено"
     def __post_init__(self):
         if not self.name:
             raise ValueError("Device is not has name!")
-    
+        
+@dataclass
+class SmartBulb(SmartDevice):
+    brightness: int = 0
+    def toggle(self):
+        if self.is_on: 
+            self.is_on = False
+            self.brightness = 0
+            print(f"Лампочка {self.name} погасла")
+        else: 
+            self.is_on = True
+            self.brightness = 80
+            print(f"Лампочка {self.name} загорелась с яркостью {self.brightness}%")
+    def __post_init__(self):
+        if self.brightness not in range(0,101):
+            raise DeviceError( f"Яркость лампочки {self.name} вышла за пределы 0-100 ({self.brightness})")
 
-my_lamp_1 = SmartDevice("lamp_1", False)
+
+
+
+my_lamp_1 = SmartBulb("lamp_1")
 
 print(my_lamp_1)
