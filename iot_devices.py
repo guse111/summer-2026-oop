fdedeca832fd5692412dee5da7633b4f8f882c34
guse111 <evguse174@gmail.com>
2026-06-18 18:35:02 +1000

    SmartSocket

--- iot_devices.py
+++ iot_devices.py
@@ -27,19 +27,25 @@ class SmartBulb(SmartDevice):
     def toggle(self):
         if self.is_on: 
             self.is_on = False
-            self.brightness = 0
             print(f"Лампочка {self.name} погасла")
         else: 
             self.is_on = True
-            self.brightness = 80
             print(f"Лампочка {self.name} загорелась с яркостью {self.brightness}%")
     def __post_init__(self):
         if self.brightness not in range(0,101):
             raise DeviceError( f"Яркость лампочки {self.name} вышла за пределы 0-100 ({self.brightness})")
 
-
-
+@dataclass
+class SmartSocket(SmartDevice):
+    max_power: float = 20
+    def toggle(self):
+        if self.is_on: 
+            self.is_on = False
+            print(f"Розетка {self.name} выключена")
+        else: 
+            self.is_on = True
+            print(f"Розетка {self.name} подает напряжение. Лимит: {self.max_power} Вт")
 
 my_lamp_1 = SmartBulb("lamp_1")
-
-print(my_lamp_1)
+my_soket_1 = SmartSocket("soket_1", False, 50)
+print(my_soket_1)
